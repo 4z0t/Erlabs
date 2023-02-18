9f290fd8790d096b3311cbe32cb426ce831b783e
4z0t <gastec139@gmail.com>
2023-02-18 13:03:29 +0300

    add intersect

--- lab1.erl
+++ lab1.erl
@@ -1,8 +1,8 @@
 -module(lab1).
 
--export([distance/2, insert/2, drop_every/2, rle_decode/1, diagonal/1]).
+-export([distance/2, insert/2, drop_every/2, rle_decode/1, diagonal/1, intersect/2]).
 
-distance({X1, Y1}, {X2, Y2}) -> math:sqrt((math:pow(X1 - X2, 2) + math:pow(Y1 - Y2, 2))).
+distance({X1, Y1}, {X2, Y2}) -> math:sqrt(math:pow(X1 - X2, 2) + math:pow(Y1 - Y2, 2)).
 
 insert(List, X) -> lists:sort(List ++ [X]).
 
@@ -21,3 +21,7 @@ rle_decode(EncodedList) ->
     lists:append(lists:map(fun decodeElement/1, EncodedList)).
 
 diagonal(M) -> lists:map(fun({I, R}) -> lists:nth(I, R) end, lists:enumerate(M)).
+
+contains(V, L) -> lists:any(fun(E) -> V =:= E end, L).
+
+intersect(L1, L2) -> lists:filter(fun(V) -> contains(V, L1) end, L2).
