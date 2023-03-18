626d29af7a79e7ca4698ec9edaa5aaa544a5d869
Alexander <84857900+4z0t@users.noreply.github.com>
2023-03-18 11:22:58 +0300

    second impl

--- lab3.erl
+++ lab3.erl
@@ -6,6 +6,11 @@
     dict_remove/2,
     dict_values/1,
     dict_keys/1,
+    dict2_insert/3,
+    dict2_find/2,
+    dict2_remove/2,
+    dict2_values/1,
+    dict2_keys/1,
     flatten/1
 ]).
 
@@ -15,6 +20,13 @@ dict_find(D, K) -> lists:nth(1, lists:filter(fun({X, _}) -> X =:= K end, D)).
 dict_values(D) -> lists:map(fun({_, V}) -> V end, D).
 dict_keys(D) -> lists:map(fun({K, _}) -> K end, D).
 
+%????
+dict2_remove(D, K) -> D.
+dict2_insert(D, K, V) -> D#{K := V}.
+dict2_find(D, K) -> D.
+dict2_values(D) -> D.
+dict2_keys(D) -> D.
+
 % {V, R, L}
-flatten({V})->[V];
+flatten({V}) -> [V];
 flatten({V, R, L}) -> flatten(R) ++ [V] ++ flatten(L).
