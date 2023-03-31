454815c2e55155496314b7c34704e6fd44a14d58
4z0t <gastec139@gmail.com>
2023-03-31 20:50:31 +0300

    wip lab4

--- labs/echo.erl
+++ labs/echo.erl
@@ -7,14 +7,41 @@
 ]).
 
 start() ->
-    io:fwrite("Started<0.33.0>~n", []),
-    ok.
+    ECHO = whereis(echo),
+    if
+        ECHO /= undefined ->
+            io:fwrite("Process is already started!~n"),
+            error;
+        true ->
+            PID = spawn(fun thread/0),
+            register(echo, PID),
+            io:fwrite(
+                "Started~n~p", [PID]
+            ),
+            ok
+    end.
 
 print(Term) ->
-    io:fwrite("~s~n", [Term]),
+    echo ! Term,
     ok.
 
 stop() ->
-    io:fwrite("Stopped!~n"),
-    halt(),
-    ok.
+    PID = whereis(echo),
+    if
+        PID == undefined ->
+            io:fwrite("Process wasnt started!~n"),
+            error;
+        true ->
+            echo ! stop,
+            io:fwrite("Stopped!~n"),
+            ok
+    end.
+
+thread() ->
+    receive
+        stop ->
+            void;
+        Term ->
+            io:fwrite("~s~n", [Term]),
+            thread()
+    end.
