3e1b1069fb62b609c1b7348df7848bb220286075
4z0t <gastec139@gmail.com>
2023-05-12 20:36:33 +0300

    lab2 prolog

--- prolog/lab2.pl
+++ prolog/lab2.pl
@@ -5,7 +5,7 @@ test_1:-
     [1,2,3]==[1,2,3],
 	list([1,2]).
 
-
+% 2
 list([]).
 list([_|T]) :- list(T).
 
@@ -18,35 +18,55 @@ duplicated([H11|[H12|T1]], [H2|T2]):-
     
 stutter(L1,L2) :- duplicated(L2,L1).
 
-remove_duplicates(L1, L2):-
-    list(L1), list(L2).
-
-append([], List, List).
-append([Head|Tail], List, [Head|Rest]) :-
-    append(Tail, List, Rest).
 
 
-my_flatten_list([], [H|T], L2):-
-    my_flatten_(H, T, L2).
-my_flatten_list([H], Tail, L2):-
-    my_flatten_(H, Tail, L2).
-my_flatten_list([H|T], Tail, L2):-
-    my_flatten_(H, [T|Tail], L2).
+
+% 3
+remove_all(_, [], []).
+remove_all(X, [X|T], T1) :-
+    remove_all(X, T, T1).
+remove_all(X, [H|T], [H|T1]) :-
+    X \= H,
+    remove_all(X, T, T1).
 
 
-my_flatten_(Head, Tail, [H2|T2]):-
-    (   
-   		list(Head),
-        my_flatten_list(Head, Tail, [H2|T2])
-    )
-    ;   
-    (   
-    	Head==H2,
-        my_flatten_(Tail, T2)
-    ).
-    
-my_flatten_([],[]):-true.
-my_flatten_(L1, L2):-
- 	my_flatten_(L1,[],L2).
-    
-my_flatten(LN, L):- my_flatten_(LN,L).
+remove_duplicates([], []).
+remove_duplicates([H|T], [H|T1]) :-
+  remove_all(H, T, T2),
+  remove_duplicates(T2, T1).
+remove_duplicates([H|T], [H|T1]) :-
+  not(member(H, T)),
+  remove_duplicates(T, T1).
+
+
+
+% 4
+
+my_flatten([], []).
+my_flatten([H|T], FlatList) :-
+  is_list(H),
+  my_flatten(H, HFlat),
+  my_flatten(T, TFlat),
+  append(HFlat, TFlat, FlatList).
+my_flatten([H|T], [H|TFlat]) :-
+  not(is_list(H)),
+  my_flatten(T, TFlat).
+
+% 5
+
+add_element_to_lists(_, [], []).
+add_element_to_lists(X, [H|T], [[X|H]|T1]) :-
+  is_list(H),
+  add_element_to_lists(X, T, T1).
+add_element_to_lists(X, [H|T], [H|T1]) :-
+  not(is_list(H)),
+  add_element_to_lists(X, T, T1).
+
+gray([], []).
+gray([_], [[0],[1]]).
+gray([_|T], Code):-
+    gray(T, C),
+	add_element_to_lists(0, C, Code0),
+    add_element_to_lists(1, C, Code1),
+    append(Code0, Code1, Code).
+
